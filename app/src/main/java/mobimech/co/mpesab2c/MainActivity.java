9457263ae040f23fe328ac02199d1ed212653b89
Derrick Mungai <derrickmungai@gmail.com>
2018-05-16 14:42:12 +0300

    First test in activity. Added internet permission to manifest. Works fine

--- app/src/main/java/mobimech/co/mpesab2c/MainActivity.java
+++ app/src/main/java/mobimech/co/mpesab2c/MainActivity.java
@@ -1,21 +1,37 @@
 package mobimech.co.mpesab2c;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import ke.co.mobimech.mpesab2c2.API.Models.AccessToken;
+import ke.co.mobimech.mpesab2c2.API.Models.B2CPaymentRequest;
+import ke.co.mobimech.mpesab2c2.API.Models.B2CPaymentResponse;
+import ke.co.mobimech.mpesab2c2.Mpesa;
+import ke.co.mobimech.mpesab2c2.MpesaLib;
 
 public class MainActivity extends AppCompatActivity {
+
+    Mpesa mpesa;
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        ButterKnife.bind(this);
         setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
@@ -24,6 +40,21 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+            }
+        });
+
+        mpesa=Mpesa.with("oTyqtS9FNz2pSGRagaam5Kw2PkInboUF", "Bkf6Aok2zYx6H92i", new MpesaLib<AccessToken>() {
+            @Override
+            public void onResult(@NonNull AccessToken accessToken) {
+                Toast.makeText(MainActivity.this, "TOKEN : " + accessToken.getAccess_token(), Toast.LENGTH_SHORT).show();
+                Log.wtf(MainActivity.this.getClass().getSimpleName(), accessToken.getAccess_token());
+
+            }
+
+            @Override
+            public void onError(String error) {
+                Toast.makeText(MainActivity.this, error,Toast.LENGTH_SHORT).show();
+
             }
         });
     }
@@ -48,5 +79,36 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @OnClick(R.id.pay)
+    public void makePayment(){
+        B2CPaymentRequest request= new B2CPaymentRequest(
+                "",
+                "",
+                "",
+                "",
+                "",
+                "",
+                "",
+                "",
+                "",
+                ""
+        );
+
+        mpesa.B2CMpesaPayment(request, new MpesaLib<B2CPaymentResponse>() {
+            @Override
+            public void onResult(@NonNull B2CPaymentResponse b2CPaymentResponse) {
+                Log.wtf(MainActivity.this.getClass().getSimpleName(), b2CPaymentResponse.ResponseDescription);
+
+            }
+
+            @Override
+            public void onError(String error) {
+                Log.wtf(MainActivity.this.getClass().getSimpleName(), error);
+
+            }
+        });
+
     }
 }
